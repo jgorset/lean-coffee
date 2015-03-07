6dece132f6dc25bec3ca6a44a87235fc10a8d6e2
Johannes Gorset <jgorset@gmail.com>
2015-03-07 15:43:36 +0100

    Create topics resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :topics
+
+  root to: 'topics#index'
 end
---
+++ spec/controllers/topics_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TopicsController, type: :controller do
+
+end
