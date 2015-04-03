213fccf936763e77074ed4da801438c0e06c2383
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2015-04-04 00:15:25 +0200

    Created constraint rake task

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,26 @@
 
 ActiveRecord::Schema.define(version: 20150307235927) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "status"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "status"
     t.integer  "votes",      default: 0, null: false
+    t.integer  "session_id"
   end
 
+  add_index "topics", ["session_id"], name: "index_topics_on_session_id", using: :btree
+
+  add_foreign_key "topics", "sessions"
 end
