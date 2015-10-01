da63ab02b857769ce2fe05c8e94efa7459a0eff1
Richard <richard@hyper.no>
2015-10-01 14:07:06 +0200

    Add cascade rule to Room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ActiveRecord::Base
-  has_many :topics
+  has_many :topics, :dependent => :delete_all
 
   validates :name, presence: true
   validates :slug, presence: true, uniqueness: true
