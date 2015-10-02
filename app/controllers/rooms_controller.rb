ace9b3fe576b7af2a5f5a48fc9a9637bbe91711a
Richard <richard@hyper.no>
2015-10-02 18:11:32 +0200

    Add notifications for rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -22,6 +22,7 @@ class RoomsController < ApplicationController
 
     respond_to do |format|
       if @room.save
+        push_new_room @room
         format.html { redirect_to @room, notice: 'Room was successfully created.' }
         format.json { render :show, status: :created, location: @room }
       else
@@ -34,6 +35,7 @@ class RoomsController < ApplicationController
   def update
     respond_to do |format|
       if @room.update(room_params)
+        push_updated_room @room
         format.html { redirect_to root_url, notice: 'Room was successfully updated.' }
         format.json { render :show, status: :ok, location: @room }
       else
@@ -46,6 +48,7 @@ class RoomsController < ApplicationController
   def destroy
     @room.destroy
     respond_to do |format|
+      push_delete_room @room
       format.html { redirect_to root_url, notice: 'Room was successfully destroyed.' }
       format.json { head :no_content }
     end
@@ -55,6 +58,18 @@ class RoomsController < ApplicationController
     topics = @room.topics.order('created_at DESC')
     topics.where!('title ilike ?', "%#{params[:q]}%") if params[:q]
     @grouped_topics = topics.group_by{ |t| t.created_at.to_date }
+  end
+
+  def push_new_room room
+    Pusher.trigger 'rooms', 'new_room', room, socket_id: params[:socket_id]
+  end
+
+  def push_updated_room room
+    Pusher.trigger 'rooms', 'updated_room', room, socket_id: params[:socket_id]
+  end
+
+  def push_delete_room room
+    Pusher.trigger 'rooms', 'deleted_room', room, socket_id: params[:socket_id]
   end
 
   private
